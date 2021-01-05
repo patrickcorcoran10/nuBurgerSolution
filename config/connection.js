8eb5ab71929fb3894a708c9901285b137a1ff77c
Patty C <patrickcorcoran10@gmail.com>
2021-01-05 17:20:06 -0600

    commit with connection js file adjusted

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,23 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.send.JAWSDB_URL)
+} else {
 
 
-connection.connect(function(err) {
+
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+
+}
+
+
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
