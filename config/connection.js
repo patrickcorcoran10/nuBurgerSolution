1feb1a8531896bd9c5775adce5a6a639372b99c3
Patty C <patrickcorcoran10@gmail.com>
2021-01-05 17:40:39 -0600

    commit with connection file change

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,8 @@
 var mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.send.JAWSDB_URL)
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
-
-
-
   var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
